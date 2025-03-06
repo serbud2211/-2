163e445065ef10ae80466ff943057c1e05cec56d
BudarovaSeredenko <katabudarova@mail.ru>
2025-03-06 14:28:19 +0700

    Signed-off-by: BudarovaSeredenko <katabudarova@mail.ru>

--- ser.java
+++ ser.java
@@ -7,7 +7,7 @@ public class ser {
         int v = Math.max(b,c);
         int max2 = a / v;
         int max1 = (a - (max2 * v));
-        System.out.println( max1);
+        System.out.println( max1+"fa");
         System.out.println( max2);
         System.out.println( "Hello");
         System.out.println( max1 - b);
