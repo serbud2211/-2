efe63c726a14d661132cdcf1eae8fd0738a58c0d
BudarovaSeredenko <katabudarova@mail.ru>
2025-03-06 14:23:59 +0700

    Initial commit.

---
+++ ser.java
@@ -0,0 +1,14 @@
+public class ser {
+    public static void main(String[] args) {
+
+        int a = 600;
+        int b = 4;
+        int c = 6;
+        int v = Math.max(b,c);
+        int max2 = a / v;
+        int max1 = (a - (max2 * v));
+        System.out.println( max1);
+        System.out.println( max2);
+        System.out.println( max1 - b);
+    }
+}
